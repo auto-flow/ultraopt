d4707084f37f5482563ddafee8a8b0e44f0a403c
TQCAI <1468632627@qq.com>
2020-12-17 11:12:58 +0800

    coding fmin

--- ultraopt/fmin.py
+++ ultraopt/fmin.py
@@ -3,9 +3,9 @@
 # @Author  : qichun tang
 # @Date    : 2020-12-17
 # @Contact    : tqichun@gmail.com
-from typing import Callable, Union, Optional
+from typing import Callable, Union, Optional, List
 
-from ConfigSpace import ConfigurationSpace
+from ConfigSpace import ConfigurationSpace, Configuration
 
 from ultraopt.config_generators.base_cg import BaseConfigGenerator
 
@@ -15,8 +15,12 @@ def fmin(
         eval_func: Callable,
         config_space: Union[ConfigurationSpace, dict],
         config_generator: Union[BaseConfigGenerator, str] = "TPE",
-        initial_points:Union[None,List]=None,
-        n_jobs=1,
+        initial_points: Union[None, List[Configuration], List[dict]] = None,
         random_state=42,
         n_iterations=100,
-)
+        n_jobs=1,
+        budgets: Optional[List[float]] = None,
+        stage_configs: Optional[List[float]] = None,
+        multi_fidelity: Optional[str] = None,
+):
+    pass
